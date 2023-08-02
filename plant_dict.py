eb7aa95a79e24b07a62e2b2049f20fe8ace7c189
Dcrisp <daniel.crisp1994@gmail.com>
2023-08-02 21:11:38 +0100

    incorporated a read in for new csv

--- plant_dict.py
+++ plant_dict.py
@@ -6,13 +6,17 @@
 #input log to check last water
 #real time date tracker
 #water frequency
-import datetime
+import pandas as pd #for working with data
+from pathlib import Path #working with file paths
+
+data_file_path = Path( "data", "house_plant_inventory.csv")
+plant_list = pd.read_csv(data_file_path)
+
+import datetime #working with live date/time
 current_time = datetime.datetime.now()
 print("Hi Daniel, the time now is:",current_time)
-house_plant_inventory = {
 
-}
-def plant_id():
+def plant_id(house_plant_inventory):
     plant_name = input('What is the name of your new plant? ')
     print(f'A {plant_name}! good choice')
     water_sched = input('How frequently (days) does your mew plant require water? ')
@@ -20,5 +24,7 @@ def plant_id():
     house_plant_inventory[plant_name] = water_sched
     print(f'I have added your {plant_name} to your list of house plants, here is your full plant list!')
     print(house_plant_inventory)
-plant_id()
+plant_id(plant_list)
 # adds a plant to the list but need to figure out how to save this addition
+
+
