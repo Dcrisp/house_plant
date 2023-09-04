441c50683e4ae17500ef1693a7e249e190ec82a4
Dcrisp <daniel.crisp1994@gmail.com>
2023-09-04 17:55:06 +0100

    adding in import csv to add rows to csv file

--- plant_dict.py
+++ plant_dict.py
@@ -8,13 +8,17 @@
 #water frequency
 import pandas as pd #for working with data
 from pathlib import Path #working with file paths
+import csv
 
 data_file_path = Path( "data", "house_plant_inventory.csv")
 plant_list = pd.read_csv(data_file_path)
+house_plant_dict = [
 
+]
 import datetime #working with live date/time
 current_time = datetime.datetime.now()
 print("Hi Daniel, the time now is:",current_time)
+
 
 def plant_id(house_plant_inventory):
     plant_name = input('What is the name of your new plant? ')
@@ -23,8 +27,19 @@ def plant_id(house_plant_inventory):
     print((f'okay I will remind you every {water_sched} days to water your {plant_name}'))
     house_plant_inventory[plant_name] = water_sched
     print(f'I have added your {plant_name} to your list of house plants, here is your full plant list!')
+    house_plant_dict[plant_name, water_sched]
+    file = open("house_plant_inventory.csv", 'a', newline='')
+    writer = csv.writer(file)
+    writer.writerows(house_plant_dict)
+    file.close()
     print(house_plant_inventory)
+
+
+
 plant_id(plant_list)
+
+
+
 # adds a plant to the list but need to figure out how to save this addition
 # need to add lines to rows not columns
 
